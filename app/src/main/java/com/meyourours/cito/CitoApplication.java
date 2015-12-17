6d1bc1938fd0280e0ae8f19103816f5273a5ad37
Superjewew <norman.lie@mediavisi.com>
2015-12-17 14:47:22 +0700

    Add analytics features - Update build.gradle for dependency - Update manifest for dependency - Add new files - Create method getDefaultTracker()

--- app/src/main/java/com/meyourours/cito/CitoApplication.java
+++ app/src/main/java/com/meyourours/cito/CitoApplication.java
@@ -2,6 +2,9 @@ package com.meyourours.cito;
 
 import android.app.Application;
 
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
@@ -16,5 +19,20 @@ public class CitoApplication extends Application {
                         .setFontAttrId(R.attr.fontPath)
                         .build()
         );
+    }
+
+    private Tracker mTracker;
+
+    /**
+     * Gets the default {@link Tracker} for this {@link Application}.
+     * @return tracker
+     */
+    synchronized public Tracker getDefaultTracker() {
+        if (mTracker == null) {
+            GoogleAnalytics analytics = GoogleAnalytics.getInstance(this);
+            // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+            mTracker = analytics.newTracker(R.xml.global_tracker);
+        }
+        return mTracker;
     }
 }
